352fbedb2e92eecce305b9b0098ea60c3de588d3
Gaurav <electrovillaofficial@gmail.com>
2024-10-01 01:54:44 +0530

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Home />
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/community" element={<Community />} />
           <Route path="/events" element={<Events />} />
--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,13 @@ export default function about() {
   return (
     <div className='h-screen w-screen'>
       <p className='text-4xl'>Hello this is about Page!</p>
-      
+      <p>Since 2018, we’ve been Uttarakhand’s
+        first recognized incubator, driving
+        innovation and entrepreneurship. We
+        help overcome challenges, attract
+        talent and investment, and
+        strengthen the local
+        economy.</p>
     </div>
   )
 }
