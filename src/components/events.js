3a9fb8d291032c20f8aa76a86a186471894f3976
Gaurav <electrovillaofficial@gmail.com>
2024-10-02 01:01:49 +0530

    updated-eventPage_and_home

--- src/components/events.js
+++ src/components/events.js
@@ -1,9 +1,102 @@
-import React from 'react'
+import React from "react"
+import Header from './header'
 
-export default function eventPage() {
+import image1 from '../assets/1.jpg';
+import image2 from '../assets/2.jpg';
+import image3 from '../assets/3.jpg';
+import image4 from '../assets/4.jpg';
+import image5 from '../assets/5.jpg';
+import image6 from '../assets/6.jpg';
+import logo from '../assets/logo.png';
+
+export default function Event() {
+
+  const callouts = [
+    {
+      name: 'Event 1',
+      description: '',
+      imageSrc: image1,
+      imageAlt: 'img1',
+      href: '#',
+    },
+    {
+      name: 'Event 2',
+      description: '',
+      imageSrc: image2,
+      imageAlt: 'img2',
+      href: '#',
+    },
+    {
+      name: 'Event 3',
+      description: '',
+      imageSrc: image3,
+      imageAlt: 'img3',
+      href: '#',
+    },
+    {
+      name: 'Event 4',
+      description: '',
+      imageSrc: image4,
+      imageAlt: 'img4',
+      href: '#',
+    },
+    {
+      name: 'Event 5',
+      description: '',
+      imageSrc: image5,
+      imageAlt: 'img5',
+      href: '#',
+    },
+    {
+      name: 'Event 6',
+      description: '',
+      imageSrc: image6,
+      imageAlt: 'img6',
+      href: '#',
+    },
+  ]
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <div className="Navigation">
+        <Header />
+      </div>
+      <div className="bg-gray-100 rounded-xl -z-10 h-screen w-screen lg:overflow-hidden">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl py-16 lg:max-w-none pb-0">
+            <div className="flex items-center gap-5 justify-between py-10 ">
+              <div>
+                <h2 className="text-5xl font-bold text-gray-900 poppins-bold">Events</h2>
+                <p className="poppins-semibold sm:text-lg lg:text-xl leading-8 my-5">Attend our interactive workshops, panel discussions, <br />
+                  pitch nights, and networking mixers to enhance your <br />
+                  skills, grow your venture, and expand your community.</p>
+              </div>
+              <img className="absolute top-0  right-0 m-5 sm:h-20 h-14" src={logo} alt="logo" />
+            </div>
+
+            <div className="mt-10 space-y-12 lg:grid lg:grid-cols-3 lg:gap-5 lg:space-y-0 relative bottom-20 bg-slate-200 p-5 pb-2 rounded-xl">
+              {callouts.map((callout) => (
+                <div key={callout.name} className="group relative">
+                  <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white aspect-auto group-hover:opacity-75 sm:h-64">
+                    <a href={callout.href}><img
+                      src={callout.imageSrc}
+                      alt={callout.imageAlt}
+                      className="h-full w-full object-cover object-center"
+                    />
+                    </a>
+                  </div>
+                  <h3 className="flex justify-center mt-2 text-sm text-gray-500 poppins-semibold">
+                    <a href={callout.href}>
+                      <span className="absolute inset-0" />
+                      {callout.name}
+                    </a>
+                  </h3>
+                  <p className="text-base font-semibold text-gray-900">{callout.description}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,38 +1,39 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import BG from '../assets/bg.jpg'
-import Header from './header'
+import { Link } from 'react-router-dom'
+import BG from '../assets/bg/BG.jpg'
+import logo from '../assets/logo.png'
+import EventPage from './events'
 
 export default function home() {
   function slideUp() {
     const page = document.getElementById('first-slide')
-    const nav = document.getElementById('content')
+    const nav = document.getElementById('Navigation')
     page.classList.add('translate-y-[-100%]', 'duration-500')
     setTimeout(() => {
-      nav.classList.remove('hidden', 'translate-x-0')
-      nav.classList.add('tranlate-x-[100]')
+      nav.classList.remove('hidden')
     }, 320)
   }
   return (
     <>
-        <div className="Home-page-container h-screen w-screen relative overflow-hidden z-10" id='first-slide'>
-          <div className='h-screen w-screen bg-gradient-to-t from-black/80'>
-            <img src={BG} alt='bg' className='h-screen w-screen absolute object-cover right-0 left-0 -z-10'></img>
-            <img src='https://tbi.geu.ac.in/assets/tbiLogo-7wOxXMot.png' alt='TBI' className='absolute top-0  right-0 m-5 h-20' ></img>
+      <div className="Home-page-container h-screen w-screen absolute z-20" id='first-slide'>
+        <div className='h-screen w-screen bg-gradient-to-t from-black/80'>
+          <img src={BG} alt='bg' className='h-screen sm:w-screen absolute object-cover right-0 left-0 -z-10'></img>
+          <img src={logo} alt='SIC_Logo' className='absolute top-0  right-0 m-5 sm:h-20 h-14' ></img>
 
-            <div className='flex justify-center items-center h-screen w-screen'>
-              <div className='grid grid-cols-1 gap-5'>
-                <h1 className='text-8xl poppins-bold text-white relative bottom-5'>SIC GEHU</h1>
-                <div className='flex justify-center'>
-                  <button onClick={slideUp} className='Get-started-btn w-fit shadow-sm shadow-gray-700 p-4 text-xl bg-slate-300 poppins-semibold rounded-full hover:bg-slate-500 hover:text-slate-50 animate-pulse duration-1000 hover:animate-none relative bottom-5'>Get Started</button>
-                </div>
+          <div className='h-screen w-screen flex justify-center items-center'>
+            <div className='grid grid-cols-1 gap-5'>
+              <h1 className='sm:text-8xl text-4xl poppins-bold text-white relative bottom-5'>SIC GEHU</h1>
+              <div className='flex justify-center'>
+                <button onClick={slideUp} className='Get-started-btn w-fit shadow-sm shadow-gray-700 p-4 text-xl bg-slate-300 poppins-semibold rounded-full hover:bg-slate-500 hover:text-slate-50 animate-pulse duration-1000 hover:animate-none relative bottom-5'>Get Started</button>
               </div>
             </div>
+          </div>
 
+          <div className='Text-content'>
             <div className='About absolute right-0 bottom-0 m-5 z-30'>
-              <div className='bg-white/20 h-fit w-fit p-6 border rounded-3xl'>
-                <div className='poppins-semibold text-white text-xl'>
-                  <div className='poppins-bold justify-start flex m-2'>About Us</div>
+              <div className='bg-white/20 h-fit w-fit p-4 border rounded-3xl'>
+                <div className='poppins-semibold text-white lg:text-xl md:text-lg text-[8px]'>
+                  <div className='poppins-bold justify-start flex mb-2'>About Us</div>
                   <div>Since 2018, we’ve been Uttarakhand’s</div>
                   <div>first recognized incubator, driving</div>
                   <div>innovation and entrepreneurship. We</div>
@@ -41,8 +42,8 @@ export default function home() {
                   <div>strengthen the local</div>
                   <div>economy.</div>
                   <Link to={'/about'}>
-                    <button className='absolute right-0 bottom-3 mr-5 border rounded-full p-5 rotate-180'>
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="#FFF" className="size-10">
+                    <button className='absolute right-0 bottom-3 mr-5 border rounded-full md:p-5 lg:p-5 p-2 rotate-180'>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="#FFF" className="lg:size-10 md:size-8 size-4">
                         <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 4.5 15 15m0 0V8.25m0 11.25H8.25" />
                       </svg>
                     </button>
@@ -51,16 +52,17 @@ export default function home() {
               </div>
             </div>
 
-            <div className='Text-content absolute bottom-0 left-0 text-white'>
-              <div className='bg-black w-fit p-6 poppins-regular text-3xl rounded-r-3xl rounded-b-none rounded-l-none'>Incubating your <div>dreams into reality</div></div>
-              <div className='bg-black w-fit p-6 poppins-bold text-8xl rounded-r-3xl  rounded-b-none rounded-l-none'>Start Up</div>
-              <div className='bg-black w-fit p-6 poppins-bold text-8xl rounded-r-3xl  rounded-b-none rounded-l-none'>Incubation Cell</div>
+            <div className='absolute bottom-0 left-0 text-white'>
+              <div className='bg-black w-fit p-6 md:p-5 sm:p-4 poppins-regular lg:text-2xl md:text-sm text-[8px] rounded-r-2xl rounded-b-none rounded-l-none'>Incubating your <p>dreams into reality</p></div>
+              <div className='bg-black w-fit p-6 md:p-5 sm:p-4 poppins-bold lg:text-8xl md:text-4xl text-xl rounded-r-2xl  rounded-b-none rounded-l-none'>Start Up</div>
+              <div className='bg-black w-fit p-6 md:p-5 sm:p-4 poppins-bold lg:text-8xl md:text-4xl text-xl rounded-r-2xl  rounded-b-none rounded-l-none'>Incubation Cell</div>
             </div>
           </div>
-        </div >
-        <div className='z-0' id='content'>
-          <Header />
         </div>
+      </div >
+      <div className='z-0' id='Navigation'>
+        <EventPage />
+      </div>
     </>
   )
 }
